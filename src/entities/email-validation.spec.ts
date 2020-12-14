8a8c0c38726449973843b3bcd345c9dc8b10904d
Flavia Ichikura <flavia.ichikura@gmail.com>
2020-12-14 03:18:56 -0300

    test: validate emails with domain larger than 255 chars

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -29,6 +29,13 @@ describe('Email validation', () => {
 })
 
 describe('Email validation', () => {
+  test('should not accept domain part larger than 255 chars', () => {
+    const email = 'local@' + 'd'.repeat(128) + '.' + 'd'.repeat(127)
+    expect(Email.validate(email)).toBeFalsy()
+  })
+})
+
+describe('Email validation', () => {
   test('should not accept local part larger than 64 chars', () => {
     const email = 'l'.repeat(65) + '@email.com'
     expect(Email.validate(email)).toBeFalsy()
