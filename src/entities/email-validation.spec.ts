a00c8c2e9747fdab55f54337bbe5aaa107e072a1
Flavia Ichikura <flavia.ichikura@gmail.com>
2020-12-15 21:07:13 -0300

    refactor: only one describe for email validation tests

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -5,37 +5,27 @@ describe('Email validation', () => {
     const email = null
     expect(Email.validate(email)).toBeFalsy()
   })
-})
 
-describe('Email validation', () => {
   test('should not accept empty strings', () => {
     const email = ''
     expect(Email.validate(email)).toBeFalsy()
   })
-})
 
-describe('Email validation', () => {
   test('should accept valid email', () => {
     const email = 'any@email.com'
     expect(Email.validate(email)).toBeTruthy()
   })
-})
 
-describe('Email validation', () => {
   test('should not accept strings larger than 320 chars', () => {
     const email = 'l'.repeat(64) + '@' + 'd'.repeat(128) + '.' + 'd'.repeat(127)
     expect(Email.validate(email)).toBeFalsy()
   })
-})
 
-describe('Email validation', () => {
   test('should not accept domain part larger than 255 chars', () => {
     const email = 'local@' + 'd'.repeat(128) + '.' + 'd'.repeat(127)
     expect(Email.validate(email)).toBeFalsy()
   })
-})
 
-describe('Email validation', () => {
   test('should not accept local part larger than 64 chars', () => {
     const email = 'l'.repeat(65) + '@email.com'
     expect(Email.validate(email)).toBeFalsy()
