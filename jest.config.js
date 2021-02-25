f963a5c252ba91dbb0573be9a380b5e3ab09d4d4
Flavia Ichikura <flavia.ichikura@gmail.com>
2021-02-25 00:20:08 -0300

    chore: configure jest mongodb

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  preset: '@shelf/jest-mongodb',
+
   // A list of paths to directories that Jest should use to search for files in
   roots: ['<rootDir>/test'],
 
@@ -10,8 +12,6 @@ module.exports = {
     '!**/test/**',
     '!**/config/**'
   ],
-
-  testEnvironment: 'node',
 
   transform: {
     '.+\\.ts$': 'ts-jest'
