09e5a54dfeef23b61824a7529b0d39677edb955f
Flavia Ichikura <flavia.ichikura@gmail.com>
2021-02-25 01:44:35 -0300

    test: add user and verify existence using mongodb

---
+++ test/external/repositories/mongodb/mongodb-user-repository.spec.ts
@@ -0,0 +1,26 @@
+import { MongoHelper } from '@/external/repositories/mongodb/helper'
+import { MongodbUserRepository } from '@/external/repositories/mongodb'
+
+describe('Mongo User repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeAll(async () => {
+    MongoHelper.clearCollection('users')
+  })
+
+  test('when user is added, it should exist', async () => {
+    const userRepository = new MongodbUserRepository()
+    const user = {
+      name: 'any_name',
+      email: 'any@mail.com'
+    }
+    await userRepository.add(user)
+    expect(await userRepository.exists(user)).toBeTruthy()
+  })
+})
